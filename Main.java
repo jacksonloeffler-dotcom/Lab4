d97fa837331f412f4c2900c5a293fcedcf6b7def
LAB4804WD03 <Jackson.Loeffler@gmail.com>
2026-06-18 09:52:29 -0700

    Lab4.

--- Main.java
+++ Main.java
@@ -7,16 +7,26 @@ public class Main
 	public static void main(String[] args)
 	{
 		// DECLARATION SECTION
-
+		double avgDacaTaxContribution;
+		int avgDacaTaxContributionRounded;
+		String stateMostDaca;
 		// INITIALIZATION SECTION
-		
+		char stateMostDaca1 = 'a';
+		char stateMostDaca2 = 'c';
+		int numDacaRecipients = 700000;
+		long dacaFederalTaxRevenue = 60000000000L;
 		// INPUT SECTION
 		// N/A (no input for this lab)
-		
-		// CALCULATION & PROCESSING SECTION
 
-		
+		// CALCULATION & PROCESSING SECTION
+		avgDacaTaxContribution = (double) dacaFederalTaxRevenue / numDacaRecipients;
+		avgDacaTaxContributionRounded = (int) avgDacaTaxContribution;
+		stateMostDaca1 = (char) (stateMostDaca1 - 32);
+		stateMostDaca2 = (char) (stateMostDaca2 - 32);
+		stateMostDaca = "" + stateMostDaca2 + stateMostDaca1;
 		// OUTPUT SECTION
-		
+		System.out.println("Average DACA-recipient Tax Contribution: $" + avgDacaTaxContribution);
+		System.out.println("Rounded down to nearest whole dollar: $" + avgDacaTaxContributionRounded);
+		System.out.println("State with most DACA recipients:" + stateMostDaca);
 	}
 }
